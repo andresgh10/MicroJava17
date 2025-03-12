1a17c40656b86dcd9b51aa60b8578c3fb5d9b6fd
andresgh10 <andresgh369@gmail.com>
2025-03-12 16:38:55 -0300

    se añade librerias banco nexus

--- src/main/java/cl/bch/cloud/ms/concepto/jdbc/repositories/CreditosRepository.java
+++
@@ -1,8 +0,0 @@
-package cl.bch.cloud.ms.concepto.jdbc.repositories;
-
-import cl.bch.cloud.ms.concepto.jdbc.entities.CreditosEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreditosRepository extends JpaRepository<CreditosEntity,String> {
-
-}
